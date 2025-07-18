6d80030f2c96f493ef189587da14648b939a0ef8
xuewanyun <xuewanyun@yzw.cn>
2025-07-18 13:59:47 +0800

    feat: init初始化

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-red-100">12</div>
+    </>
+  );
+}
+
+export default App;
